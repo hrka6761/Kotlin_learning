967d978383d71eb353c9e87f5c6fc4b9efb9ca1e
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-11-02 12:25:27 +0330

    Creating MainContainer.kt

--- app/src/main/java/ir/hrka/kotlin/MainActivity.kt
+++ app/src/main/java/ir/hrka/kotlin/MainActivity.kt
@@ -2,12 +2,16 @@ package ir.hrka.kotlin
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import ir.hrka.kotlin.cheatSheet.*
 import ir.hrka.kotlin.helpers.Log.printYellow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent { AppContent() }
 
 ///////// 1) Basic /////////////////////////////////////////////////////////////////////////////////
 
---
+++ app/src/main/java/ir/hrka/kotlin/MainContainer.kt
@@ -0,0 +1,25 @@
+package ir.hrka.kotlin
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import ir.hrka.kotlin.ui.theme.KotlinTheme
+
+@Composable
+fun AppContent() {
+    val navHostController = rememberNavController()
+    val activity = (LocalContext.current as MainActivity)
+
+    KotlinTheme {
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun AppContentPreview(modifier: Modifier = Modifier) {
+
+}
