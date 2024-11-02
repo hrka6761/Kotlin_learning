3fd8d53db3ad6cd0763f7a2fa04fb47833fb21bd
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-11-02 18:05:49 +0330

    Adding BASE_URL in Constants

--- app/src/main/java/ir/hrka/kotlin/core/utilities/Constants.kt
+++ app/src/main/java/ir/hrka/kotlin/core/utilities/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val TAG = "KotlinCheatSheetProject"
     const val SPLASH_SCREEN = "splash"
     const val HOME_SCREEN = "home"
+    const val BASE_URL = "https://api.github.com/repos/hrka6761/Kotlin_cheat_sheet/contents/"
 }
