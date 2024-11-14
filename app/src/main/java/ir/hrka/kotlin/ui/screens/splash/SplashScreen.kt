82c6e85f56ddcfe776115ca2b4b15c97a17e6410
HAMIDREZA <hrka6761gmail.com>
2024-11-14 12:33:47 +0330

    Adding Scaffold in the SplashScreen

--- app/src/main/java/ir/hrka/kotlin/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/ir/hrka/kotlin/ui/screens/splash/SplashScreen.kt
@@ -54,6 +54,8 @@ import ir.hrka.kotlin.core.utilities.Resource
 import ir.hrka.kotlin.core.utilities.Screen.Home
 import kotlinx.coroutines.delay
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.ui.res.stringResource
 import androidx.core.content.FileProvider
 import ir.hrka.kotlin.core.utilities.Constants.NEW_VERSION_DOWNLOADED
@@ -76,193 +78,201 @@ fun SplashScreen(activity: MainActivity, navHostController: NavHostController) {
     val installLauncher = rememberLauncherForActivityResult(StartActivityForResult()) {}
 
 
-    ConstraintLayout(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        val (logo, title, authorImg, authorName, progressBar, snackBar) = createRefs()
-
-        Image(
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) { innerPaddings ->
+        ConstraintLayout(
             modifier = Modifier
-                .width(100.dp)
-                .height(100.dp)
-                .constrainAs(logo) {
-                    top.linkTo(parent.top)
-                    bottom.linkTo(parent.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                },
-            painter = painterResource(R.drawable.logo),
-            contentDescription = null,
-        )
+                .fillMaxSize()
+                .padding(innerPaddings)
+        ) {
+            val (logo, title, authorImg, authorName, progressBar, snackBar) = createRefs()
 
-        Text(
-            modifier = Modifier
-                .constrainAs(title) {
-                    top.linkTo(logo.bottom, margin = 8.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                },
-            text = stringResource(R.string.splash_screen_title)
-        )
-
-        Text(
-            modifier = Modifier
-                .constrainAs(authorName) {
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom, margin = 16.dp)
-                },
-            text = stringResource(R.string.splash_screen_author_name),
-            fontSize = 8.sp
-        )
-
-        CircularProgressIndicator(
-            modifier = Modifier
-                .width(35.dp)
-                .height(35.dp)
-                .alpha(
-                    if (newVersionState == NEW_VERSION_AVAILABLE ||
-                        newVersionState == NEW_VERSION_CONTINUE ||
-                        newVersionState == NEW_VERSION_DOWNLOADING ||
-                        newVersionState == NEW_VERSION_NOT_INSTALL_PERMISSION ||
-                        newVersionState == NEW_VERSION_INSTALLING
-                    ) 0f else 1f
-                )
-                .constrainAs(progressBar) {
-                    bottom.linkTo(authorName.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                },
-            strokeWidth = 1.dp
-        )
-
-        Image(
-            modifier = Modifier
-                .width(25.dp)
-                .height(25.dp)
-                .constrainAs(authorImg) {
-                    bottom.linkTo(authorName.top, margin = 5.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                },
-            painter = painterResource(R.drawable.author),
-            contentDescription = null,
-        )
-
-        if (newVersionState == NEW_VERSION_AVAILABLE ||
-            newVersionState == NEW_VERSION_CONTINUE ||
-            newVersionState == NEW_VERSION_DOWNLOADING ||
-            newVersionState == NEW_VERSION_NOT_INSTALL_PERMISSION
-        )
-            AlertDialog(
-                modifier = Modifier.fillMaxWidth(),
-                onDismissRequest = {},
-                confirmButton = {
-                    when (newVersionState) {
-                        NEW_VERSION_AVAILABLE -> {
-                            TextButton(
-                                onClick = {
-                                    viewModel.setNewVersionDialogState(NEW_VERSION_CONTINUE)
-                                }
-                            ) {
-                                Text(
-                                    text = stringResource(R.string.new_version_dialog_update_btn),
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 16.sp
-                                )
-                            }
-                        }
-
-                        NEW_VERSION_NOT_INSTALL_PERMISSION -> {
-                            TextButton(
-                                onClick = {
-                                    val intent = Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES)
-                                        .apply {
-                                            data = Uri.parse("package:${activity.packageName}")
-                                        }
-                                    installPermissionLauncher.launch(intent)
-                                }
-                            ) {
-                                Text(
-                                    text = stringResource(R.string.new_version_dialog_allow_btn),
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 16.sp
-                                )
-                            }
-                        }
-                    }
-                },
-                dismissButton = {
-                    if (newVersionState == NEW_VERSION_AVAILABLE ||
-                        newVersionState == NEW_VERSION_NOT_INSTALL_PERMISSION
-                    )
-                        TextButton(
-                            onClick = {
-                                viewModel.setNewVersionDialogState(NEW_VERSION_CANCEL)
-                            }
-                        ) { Text(text = stringResource(R.string.new_version_dialog_cancel_btn)) }
-                },
-                icon = { Icon(painterResource(R.drawable.update), contentDescription = null) },
-                title = {
-                    when (newVersionState) {
-                        NEW_VERSION_AVAILABLE -> Text(text = stringResource(R.string.new_version_dialog_title_update))
-                        NEW_VERSION_DOWNLOADING -> Text(text = stringResource(R.string.new_version_dialog_title_downloading))
-                        NEW_VERSION_NOT_INSTALL_PERMISSION -> Text(text = stringResource(R.string.new_version_dialog_title_install_permission))
-                    }
-                },
-                text = {
-                    when (newVersionState) {
-                        NEW_VERSION_AVAILABLE -> {
-                            Text(
-                                stringResource(
-                                    R.string.new_version_dialog_desc_available,
-                                    if (appInfo.data?.versionNameSuffix == MANDATORY)
-                                        stringResource(R.string.new_version_dialog_desc_available_mandatory_update)
-                                    else
-                                        ""
-                                )
-                            )
-                        }
-
-                        NEW_VERSION_DOWNLOADING -> {
-                            Column(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .height(40.dp),
-                                verticalArrangement = Arrangement.Center,
-                                horizontalAlignment = Alignment.CenterHorizontally
-                            ) {
-                                CircularProgressIndicator(strokeWidth = 2.dp)
-                            }
-                        }
-
-                        NEW_VERSION_NOT_INSTALL_PERMISSION -> {
-                            Text(
-                                stringResource(
-                                    R.string.new_version_dialog_desc_install_permission,
-                                    if (appInfo.data?.versionNameSuffix == MANDATORY)
-                                        stringResource(R.string.new_version_dialog_desc_available_mandatory_update)
-                                    else
-                                        ""
-                                )
-                            )
-                        }
-                    }
-                },
-                shape = RoundedCornerShape(16.dp),
-                tonalElevation = 16.dp
+            Image(
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(100.dp)
+                    .constrainAs(logo) {
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                painter = painterResource(R.drawable.logo),
+                contentDescription = null,
             )
 
-        SnackbarHost(
-            modifier = Modifier
-                .constrainAs(snackBar) {
-                    bottom.linkTo(parent.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
-                .fillMaxWidth(),
-            hostState = snackBarHostState
-        )
+            Text(
+                modifier = Modifier
+                    .constrainAs(title) {
+                        top.linkTo(logo.bottom, margin = 8.dp)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                text = stringResource(R.string.splash_screen_title)
+            )
+
+            Text(
+                modifier = Modifier
+                    .constrainAs(authorName) {
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom, margin = 16.dp)
+                    },
+                text = stringResource(R.string.splash_screen_author_name),
+                fontSize = 8.sp
+            )
+
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .width(35.dp)
+                    .height(35.dp)
+                    .alpha(
+                        if (newVersionState == NEW_VERSION_AVAILABLE ||
+                            newVersionState == NEW_VERSION_CONTINUE ||
+                            newVersionState == NEW_VERSION_DOWNLOADING ||
+                            newVersionState == NEW_VERSION_NOT_INSTALL_PERMISSION ||
+                            newVersionState == NEW_VERSION_INSTALLING
+                        ) 0f else 1f
+                    )
+                    .constrainAs(progressBar) {
+                        bottom.linkTo(authorName.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                strokeWidth = 1.dp
+            )
+
+            Image(
+                modifier = Modifier
+                    .width(25.dp)
+                    .height(25.dp)
+                    .constrainAs(authorImg) {
+                        bottom.linkTo(authorName.top, margin = 5.dp)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                painter = painterResource(R.drawable.author),
+                contentDescription = null,
+            )
+
+            if (newVersionState == NEW_VERSION_AVAILABLE ||
+                newVersionState == NEW_VERSION_CONTINUE ||
+                newVersionState == NEW_VERSION_DOWNLOADING ||
+                newVersionState == NEW_VERSION_NOT_INSTALL_PERMISSION
+            )
+                AlertDialog(
+                    modifier = Modifier.fillMaxWidth(),
+                    onDismissRequest = {},
+                    confirmButton = {
+                        when (newVersionState) {
+                            NEW_VERSION_AVAILABLE -> {
+                                TextButton(
+                                    onClick = {
+                                        viewModel.setNewVersionDialogState(NEW_VERSION_CONTINUE)
+                                    }
+                                ) {
+                                    Text(
+                                        text = stringResource(R.string.new_version_dialog_update_btn),
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 16.sp
+                                    )
+                                }
+                            }
+
+                            NEW_VERSION_NOT_INSTALL_PERMISSION -> {
+                                TextButton(
+                                    onClick = {
+                                        val intent =
+                                            Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES)
+                                                .apply {
+                                                    data =
+                                                        Uri.parse("package:${activity.packageName}")
+                                                }
+                                        installPermissionLauncher.launch(intent)
+                                    }
+                                ) {
+                                    Text(
+                                        text = stringResource(R.string.new_version_dialog_allow_btn),
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 16.sp
+                                    )
+                                }
+                            }
+                        }
+                    },
+                    dismissButton = {
+                        if (newVersionState == NEW_VERSION_AVAILABLE ||
+                            newVersionState == NEW_VERSION_NOT_INSTALL_PERMISSION
+                        )
+                            TextButton(
+                                onClick = {
+                                    viewModel.setNewVersionDialogState(NEW_VERSION_CANCEL)
+                                }
+                            ) { Text(text = stringResource(R.string.new_version_dialog_cancel_btn)) }
+                    },
+                    icon = { Icon(painterResource(R.drawable.update), contentDescription = null) },
+                    title = {
+                        when (newVersionState) {
+                            NEW_VERSION_AVAILABLE -> Text(text = stringResource(R.string.new_version_dialog_title_update))
+                            NEW_VERSION_DOWNLOADING -> Text(text = stringResource(R.string.new_version_dialog_title_downloading))
+                            NEW_VERSION_NOT_INSTALL_PERMISSION -> Text(text = stringResource(R.string.new_version_dialog_title_install_permission))
+                        }
+                    },
+                    text = {
+                        when (newVersionState) {
+                            NEW_VERSION_AVAILABLE -> {
+                                Text(
+                                    stringResource(
+                                        R.string.new_version_dialog_desc_available,
+                                        if (appInfo.data?.versionNameSuffix == MANDATORY)
+                                            stringResource(R.string.new_version_dialog_desc_available_mandatory_update)
+                                        else
+                                            ""
+                                    )
+                                )
+                            }
+
+                            NEW_VERSION_DOWNLOADING -> {
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(40.dp),
+                                    verticalArrangement = Arrangement.Center,
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    CircularProgressIndicator(strokeWidth = 2.dp)
+                                }
+                            }
+
+                            NEW_VERSION_NOT_INSTALL_PERMISSION -> {
+                                Text(
+                                    stringResource(
+                                        R.string.new_version_dialog_desc_install_permission,
+                                        if (appInfo.data?.versionNameSuffix == MANDATORY)
+                                            stringResource(R.string.new_version_dialog_desc_available_mandatory_update)
+                                        else
+                                            ""
+                                    )
+                                )
+                            }
+                        }
+                    },
+                    shape = RoundedCornerShape(16.dp),
+                    tonalElevation = 16.dp
+                )
+
+            SnackbarHost(
+                modifier = Modifier
+                    .constrainAs(snackBar) {
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+                    .fillMaxWidth(),
+                hostState = snackBarHostState
+            )
+        }
     }
 
     LaunchedEffect(appInfo) {
