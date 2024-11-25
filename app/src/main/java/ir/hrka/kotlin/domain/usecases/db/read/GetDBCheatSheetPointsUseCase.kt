fa306f5926d5b750549f6842eb0cffc08dfb7ba2
Hamidreza Karami <h.karami@dariahamrah.ir>
2024-11-25 14:26:58 +0330

    Resolving bug when get points from database

--- app/src/main/java/ir/hrka/kotlin/domain/usecases/db/read/GetDBCheatSheetPointsUseCase.kt
+++ app/src/main/java/ir/hrka/kotlin/domain/usecases/db/read/GetDBCheatSheetPointsUseCase.kt
@@ -1,9 +1,10 @@
 package ir.hrka.kotlin.domain.usecases.db.read
 
+import ir.hrka.kotlin.domain.repositories.ReadDBCheatSheetRepo
 import ir.hrka.kotlin.domain.repositories.ReadGithubCheatSheetRepo
 import javax.inject.Inject
 
-class GetDBCheatSheetPointsUseCase @Inject constructor(private val readGithubCheatSheetRepo: ReadGithubCheatSheetRepo) {
+class GetDBCheatSheetPointsUseCase @Inject constructor(private val readDBCheatSheetRepo: ReadDBCheatSheetRepo) {
 
-    suspend operator fun invoke(fileName: String) = readGithubCheatSheetRepo.getCheatSheetPoints(fileName)
+    suspend operator fun invoke(fileName: String) = readDBCheatSheetRepo.getCheatSheetPoints(fileName)
 }
