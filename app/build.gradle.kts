df610fc3f58e89aecb46af9c5d964a31d6ed40c7
HAMIDREZA <hrka6761gmail.com>
2024-11-29 15:52:57 +0330

    Changing priority number of some cheatsheet files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 4
-        versionName = "3.0.5"
-        versionNameSuffix = "6"
+        versionName = "3.1.0"
+        versionNameSuffix = "non_mandatory"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
