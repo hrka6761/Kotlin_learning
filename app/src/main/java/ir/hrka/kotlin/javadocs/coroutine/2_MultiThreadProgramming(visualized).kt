f64989d6919d646668f382004b653e3d95607d96
HAMIDREZA <hrka6761gmail.com>
2025-01-11 22:33:24 +0330

    Add snippet code to multi thread programming

--- app/src/main/java/ir/hrka/kotlin/javadocs/coroutine/2_MultiThreadProgramming(visualized).kt
+++ app/src/main/java/ir/hrka/kotlin/javadocs/coroutine/2_MultiThreadProgramming(visualized).kt
@@ -1,7 +1,5 @@
 package ir.hrka.kotlin.javadocs.coroutine
 
-import ir.hrka.kotlin.core.utilities.Log.printYellow
-
 /**
  * * In the Concurrent programming, we can serve the threads To execute multi task simultaneously and independently of each other.
  * * Threads are a low-level construct managed by the operating system.
@@ -10,38 +8,72 @@ import ir.hrka.kotlin.core.utilities.Log.printYellow
  * * In the use of threads, the following should be considered:
  *    * Creating and managing threads is resource-intensive.
  *    * Threads require careful synchronization to avoid race conditions, deadlocks, and other concurrency issues.
+ * ```
+ *     fun main() {
+ *         task1()
+ *
+ *         Thread {
+ *             task2()
+ *             task3()
+ *         }.start()
+ *
+ *         Thread {
+ *             task4()
+ *             task5()
+ *         }.start()
+ *
+ *         task6()
+ *     }
+ *
+ *     private fun task1() { ... }
+ *     private fun task2() { ... }
+ *     private fun task3() { ... }
+ *     private fun task4() { ... }
+ *     private fun task5() { ... }
+ *     private fun task6() { ... }
+ * ```
  */
 
 class MultiThreadProgramming {
 
-    fun fun1() {
+    fun main() {
         task1()
-        task2()
-        task3()
+
+        Thread {
+            task2()
+            task3()
+        }.start()
+
+        Thread {
+            task4()
+            task5()
+        }.start()
+
+        task6()
     }
 
 
     private fun task1() {
-        Thread {
-            printYellow("Start task1")
-            Thread.sleep(1_000)
-            printYellow("End task1")
-        }.start()
+        Thread.sleep(1_000)
     }
 
     private fun task2() {
-        Thread {
-            printYellow("Start task2")
-            Thread.sleep(4_000)
-            printYellow("End task2")
-        }.start()
+        Thread.sleep(4_000)
     }
 
     private fun task3() {
-        Thread {
-            printYellow("Start task3")
-            Thread.sleep(2_000)
-            printYellow("End task3")
-        }.start()
+        Thread.sleep(2_000)
+    }
+
+    private fun task4() {
+        Thread.sleep(3_000)
+    }
+
+    private fun task5() {
+        Thread.sleep(1_000)
+    }
+
+    private fun task6() {
+        Thread.sleep(2_500)
     }
 }
