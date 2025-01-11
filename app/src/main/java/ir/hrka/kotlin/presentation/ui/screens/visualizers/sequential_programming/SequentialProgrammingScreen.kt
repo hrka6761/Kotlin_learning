b99718c29f3fa829db4b8c53c2d217d28bdd3686
Hamidreza Karami <h.karami@dariahamrah.ir>
2025-01-11 11:04:21 +0330

    Redesign SequentialProgrammingScreen app bar

--- app/src/main/java/ir/hrka/kotlin/presentation/ui/screens/visualizers/sequential_programming/SequentialProgrammingScreen.kt
+++ app/src/main/java/ir/hrka/kotlin/presentation/ui/screens/visualizers/sequential_programming/SequentialProgrammingScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -112,7 +113,13 @@ fun SequentialProgrammingAppBar(
     executionState: ExecutionState
 ) {
     TopAppBar(
-        title = { Text(stringResource(R.string.sequential_programming_app_bar_title)) },
+        title = {
+            Text(
+                modifier = Modifier.padding(end = 16.dp),
+                text = stringResource(R.string.sequential_programming_app_bar_title),
+                fontWeight = FontWeight.Bold
+            )
+        },
         navigationIcon = {
             IconButton(
                 onClick = { navHostController.popBackStack() }
