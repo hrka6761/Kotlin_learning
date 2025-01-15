4e4d19198386d965d3219f1c13807b3645660e06
Hamidreza Karami <h.karami@dariahamrah.ir>
2025-01-15 18:02:48 +0330

    Change restart app bar icon in visualizers screen until execution sate is not stop

--- app/src/main/java/ir/hrka/kotlin/presentation/ui/screens/visualizers/coroutine_scope_function/CoroutineScopeFunctionScreen.kt
+++ app/src/main/java/ir/hrka/kotlin/presentation/ui/screens/visualizers/coroutine_scope_function/CoroutineScopeFunctionScreen.kt
@@ -37,6 +37,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -283,6 +284,7 @@ fun CoroutineScopeFunctionScreenAppBar(
             Icon(
                 modifier = Modifier
                     .padding(end = 8.dp)
+                    .alpha(if (executionState == ExecutionState.Stop) 1f else 0.2f)
                     .clickable(enabled = executionState == ExecutionState.Stop) {
                         viewModel.restartVisualizer()
                     },
