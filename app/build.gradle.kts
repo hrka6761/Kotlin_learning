5b27d0505de2885d7e59abc1e969e2ee55d7c5e3
Hamidreza Karami <h.karami@dariahamrah.ir>
2025-01-27 16:55:35 +0330

    Create version 6.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 12
-        versionName = "6.4.0"
+        versionName = "6.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
