3f832265fdc3ade31e05ee20ef3298d1105d1950
HAMIDREZA <hrka6761gmail.com>
2025-01-30 14:39:14 +0330

    Improve SplashScreen and SplashViewModel

--- app/src/main/java/ir/hrka/kotlin/presentation/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/ir/hrka/kotlin/presentation/ui/screens/splash/SplashScreen.kt
@@ -1,5 +1,7 @@
 package ir.hrka.kotlin.presentation.ui.screens.splash
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -32,30 +34,21 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import ir.hrka.kotlin.presentation.MainActivity
 import ir.hrka.kotlin.R
-import ir.hrka.kotlin.core.utilities.Resource
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.res.stringResource
+import ir.hrka.kotlin.core.Constants.BAZAAR_URL
 import ir.hrka.kotlin.core.Constants.FORCE_UPDATE_STATE
 import ir.hrka.kotlin.core.Constants.NO_UPDATE_STATE
 import ir.hrka.kotlin.core.Constants.UPDATE_UNKNOWN_STATE
 import ir.hrka.kotlin.core.Constants.UPDATE_STATE
-import ir.hrka.kotlin.core.utilities.ExecutionState.Loading
-import ir.hrka.kotlin.core.utilities.ExecutionState.Start
 import ir.hrka.kotlin.core.utilities.ExecutionState.Stop
 import ir.hrka.kotlin.core.utilities.Screen.Home
 
 @Composable
 fun SplashScreen(activity: MainActivity, navHostController: NavHostController) {
 
-    val viewModel: SplashViewModel = hiltViewModel()
     val snackBarHostState = remember { SnackbarHostState() }
-    val executionState by viewModel.executionState.collectAsState()
-    val changelog by viewModel.changelog.collectAsState()
-    val updateState by viewModel.updateState.collectAsState()
-    val coursesVersionId by viewModel.coursesVersionId.collectAsState()
-    val kotlinVersionId by viewModel.kotlinVersionId.collectAsState()
-    val coroutineVersionId by viewModel.coroutineVersionId.collectAsState()
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -109,19 +102,6 @@ fun SplashScreen(activity: MainActivity, navHostController: NavHostController) {
                 fontSize = 8.sp
             )
 
-            CircularProgressIndicator(
-                modifier = Modifier
-                    .width(35.dp)
-                    .height(35.dp)
-                    .alpha(if (updateState == UPDATE_UNKNOWN_STATE || updateState == NO_UPDATE_STATE) 1f else 0f)
-                    .constrainAs(progressBar) {
-                        bottom.linkTo(authorName.top)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                    },
-                strokeWidth = 1.dp
-            )
-
             Image(
                 modifier = Modifier
                     .width(25.dp)
@@ -135,133 +115,104 @@ fun SplashScreen(activity: MainActivity, navHostController: NavHostController) {
                 contentDescription = null,
             )
 
-            if (updateState == UPDATE_STATE || updateState == FORCE_UPDATE_STATE)
-                AlertDialog(
-                    modifier = Modifier.fillMaxWidth(),
-                    onDismissRequest = {},
-                    confirmButton = {
-                        TextButton(
-                            onClick = { viewModel.goToUpdate(activity) }
-                        ) {
-                            Text(
-                                text = stringResource(R.string.new_version_dialog_update_btn),
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 16.sp
-                            )
-                        }
-                    },
-                    dismissButton = {
-                        TextButton(
-                            onClick = {
-                                if (updateState == FORCE_UPDATE_STATE)
-                                    activity.finish()
-                                else
-                                    navHostController.navigate(Home())
-                            }
-                        ) {
-                            Text(text = stringResource(R.string.new_version_dialog_cancel_btn))
-                        }
-                    },
-                    icon = { Icon(painterResource(R.drawable.update), contentDescription = null) },
-                    title = {
-                        Text(text = stringResource(R.string.new_version_dialog_title_update))
-                    },
-                    text = {
-                        Column {
-                            Text(
-                                text = stringResource(R.string.new_version_dialog_desc_available)
-                            )
-                            if (updateState == FORCE_UPDATE_STATE)
-                                Text(
-                                    fontSize = 16.sp,
-                                    fontWeight = FontWeight.Bold,
-                                    text = stringResource(R.string.new_version_dialog_desc_available_mandatory_update)
-                                )
-                        }
-                    },
-                    shape = RoundedCornerShape(16.dp),
-                    tonalElevation = 16.dp
-                )
-        }
-    }
-
-    LaunchedEffect(Unit) {
-        if (executionState == Start) {
-            viewModel.setExecutionState(Loading)
-            viewModel.getAppChangelog()
-        }
-    }
-
-    LaunchedEffect(changelog) {
-        if (executionState != Stop)
-            when (changelog) {
-                is Resource.Initial -> {}
-                is Resource.Loading -> {}
-                is Resource.Success -> {
-                    viewModel.getCoursesVersionId()
-                }
-
-                is Resource.Error -> {
-                    viewModel.getCoursesVersionId()
-                }
-            }
-    }
-
-    LaunchedEffect(coursesVersionId) {
-        if (executionState != Stop)
-            when (coursesVersionId) {
-                is Resource.Initial -> {}
-                is Resource.Loading -> {}
-                is Resource.Success -> {
-                    viewModel.getKotlinVersionId()
-                }
-
-                is Resource.Error -> {
-                    viewModel.getKotlinVersionId()
-                }
-            }
-    }
-
-    LaunchedEffect(kotlinVersionId) {
-        if (executionState != Stop)
-            when (kotlinVersionId) {
-                is Resource.Initial -> {}
-                is Resource.Loading -> {}
-                is Resource.Success -> {
-                    viewModel.getCoroutineVersionId()
-                }
-
-                is Resource.Error -> {
-                    viewModel.getCoroutineVersionId()
-                }
-            }
-    }
-
-    LaunchedEffect(coroutineVersionId) {
-        if (executionState != Stop)
-            when (coroutineVersionId) {
-                is Resource.Initial -> {}
-                is Resource.Loading -> {}
-                is Resource.Success -> {
-                    viewModel.initGlobalData(activity)
-                    viewModel.checkNewVersion()
-                }
-
-                is Resource.Error -> {
-                    viewModel.initGlobalData(activity)
-                    viewModel.checkNewVersion()
-                }
-            }
-    }
-
-    LaunchedEffect(updateState) {
-        if (executionState != Stop && updateState == NO_UPDATE_STATE) {
-            viewModel.setExecutionState(Stop)
-            navHostController.navigate(Home())
+            Loading(
+                modifier = Modifier
+                    .width(35.dp)
+                    .height(35.dp)
+                    .constrainAs(progressBar) {
+                        bottom.linkTo(authorName.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+            )
+            UpdateDialog(activity, navHostController)
+            StartApp(activity, navHostController)
         }
     }
 }
 
+@Composable
+fun Loading(modifier: Modifier) {
+    val viewModel: SplashViewModel = hiltViewModel()
+    val updateState by viewModel.updateState.collectAsState()
+
+    CircularProgressIndicator(
+        modifier = modifier
+            .alpha(if (updateState == UPDATE_UNKNOWN_STATE || updateState == NO_UPDATE_STATE) 1f else 0f),
+        strokeWidth = 1.dp
+    )
+}
+
+@Composable
+fun UpdateDialog(activity: MainActivity, navHostController: NavHostController) {
+    val viewModel: SplashViewModel = hiltViewModel()
+    val updateState by viewModel.updateState.collectAsState()
+
+    if (updateState == UPDATE_STATE || updateState == FORCE_UPDATE_STATE)
+        AlertDialog(
+            modifier = Modifier.fillMaxWidth(),
+            onDismissRequest = {},
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(BAZAAR_URL))
+                        activity.startActivity(intent)
+                    }
+                ) {
+                    Text(
+                        text = stringResource(R.string.new_version_dialog_update_btn),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp
+                    )
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        if (updateState == FORCE_UPDATE_STATE)
+                            activity.finish()
+                        else
+                            navHostController.navigate(Home())
+                    }
+                ) {
+                    Text(text = stringResource(R.string.new_version_dialog_cancel_btn))
+                }
+            },
+            icon = { Icon(painterResource(R.drawable.update), contentDescription = null) },
+            title = {
+                Text(text = stringResource(R.string.new_version_dialog_title_update))
+            },
+            text = {
+                Column {
+                    Text(
+                        text = stringResource(R.string.new_version_dialog_desc_available)
+                    )
+                    if (updateState == FORCE_UPDATE_STATE)
+                        Text(
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            text = stringResource(R.string.new_version_dialog_desc_available_mandatory_update)
+                        )
+                }
+            },
+            shape = RoundedCornerShape(16.dp),
+            tonalElevation = 16.dp
+        )
+}
+
+@Composable
+fun StartApp(activity: MainActivity, navHostController: NavHostController) {
+    val viewModel: SplashViewModel = hiltViewModel()
+    val executionState by viewModel.executionState.collectAsState()
+    val updateState by viewModel.updateState.collectAsState()
+
+    LaunchedEffect(updateState) {
+        viewModel.checkChangelog(activity)
+
+        if (executionState != Stop && updateState == NO_UPDATE_STATE)
+            navHostController.navigate(Home())
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/ir/hrka/kotlin/presentation/ui/screens/splash/SplashViewModel.kt
+++ app/src/main/java/ir/hrka/kotlin/presentation/ui/screens/splash/SplashViewModel.kt
@@ -1,14 +1,11 @@
 package ir.hrka.kotlin.presentation.ui.screens.splash
 
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageInfo
-import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ir.hrka.kotlin.presentation.MainActivity
-import ir.hrka.kotlin.core.Constants.BAZAAR_URL
 import ir.hrka.kotlin.core.Constants.DEFAULT_VERSION_CODE
 import ir.hrka.kotlin.core.Constants.DEFAULT_VERSION_ID
 import ir.hrka.kotlin.core.Constants.FORCE_UPDATE_STATE
@@ -16,7 +13,9 @@ import ir.hrka.kotlin.core.Constants.NO_UPDATE_STATE
 import ir.hrka.kotlin.core.Constants.UPDATE_STATE
 import ir.hrka.kotlin.core.Constants.UPDATE_UNKNOWN_STATE
 import ir.hrka.kotlin.core.utilities.ExecutionState
+import ir.hrka.kotlin.core.utilities.ExecutionState.Loading
 import ir.hrka.kotlin.core.utilities.ExecutionState.Start
+import ir.hrka.kotlin.core.utilities.ExecutionState.Stop
 import ir.hrka.kotlin.core.utilities.Resource
 import ir.hrka.kotlin.domain.entities.VersionsInfo
 import ir.hrka.kotlin.domain.usecases.git.GetChangelogFromGitUseCase
@@ -47,51 +46,138 @@ class SplashViewModel @Inject constructor(
     val updateState: StateFlow<Int> = _updateState
     private val _versionsInfo: MutableStateFlow<Resource<VersionsInfo?>> =
         MutableStateFlow(Resource.Initial())
-    val changelog: StateFlow<Resource<VersionsInfo?>> = _versionsInfo
     private val _coursesVersionId: MutableStateFlow<Resource<Int?>> =
         MutableStateFlow(Resource.Initial())
-    val coursesVersionId: StateFlow<Resource<Int?>> = _coursesVersionId
     private val _kotlinVersionId: MutableStateFlow<Resource<Int?>> =
         MutableStateFlow(Resource.Initial())
-    val kotlinVersionId: StateFlow<Resource<Int?>> = _kotlinVersionId
     private val _coroutineVersionId: MutableStateFlow<Resource<Int?>> =
         MutableStateFlow(Resource.Initial())
-    val coroutineVersionId: StateFlow<Resource<Int?>> = _coroutineVersionId
 
 
-    fun setExecutionState(state: ExecutionState) {
+    fun checkChangelog(activity: MainActivity) {
+        initChangelogResult()
+        initCoursesVersionIdResult()
+        initKotlinVersionIdResult()
+        initCoroutineVersionIdResult(activity)
+
+        if (_executionState.value == Start) {
+            setExecutionState(Loading)
+            getAppChangelog()
+        }
+    }
+
+
+    private fun initChangelogResult() {
+        viewModelScope.launch {
+            _versionsInfo.collect { result ->
+                if (_executionState.value != Stop)
+                    when (result) {
+                        is Resource.Initial -> {}
+                        is Resource.Loading -> {}
+                        is Resource.Success -> {
+                            getCoursesVersionId()
+                        }
+
+                        is Resource.Error -> {
+                            getCoursesVersionId()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun initCoursesVersionIdResult() {
+        viewModelScope.launch {
+            _coursesVersionId.collect { result ->
+                if (_executionState.value != Stop)
+                    when (result) {
+                        is Resource.Initial -> {}
+                        is Resource.Loading -> {}
+                        is Resource.Success -> {
+                            getKotlinVersionId()
+                        }
+
+                        is Resource.Error -> {
+                            getKotlinVersionId()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun initKotlinVersionIdResult() {
+        viewModelScope.launch {
+            _kotlinVersionId.collect { result ->
+                if (_executionState.value != Stop)
+                    when (result) {
+                        is Resource.Initial -> {}
+                        is Resource.Loading -> {}
+                        is Resource.Success -> {
+                            getCoroutineVersionId()
+                        }
+
+                        is Resource.Error -> {
+                            getCoroutineVersionId()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun initCoroutineVersionIdResult(activity: MainActivity) {
+        viewModelScope.launch {
+            _coroutineVersionId.collect { result ->
+                if (_executionState.value != Stop)
+                    when (result) {
+                        is Resource.Initial -> {}
+                        is Resource.Loading -> {}
+                        is Resource.Success -> {
+                            initGlobalData(activity)
+                            checkNewVersion()
+                        }
+
+                        is Resource.Error -> {
+                            initGlobalData(activity)
+                            checkNewVersion()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun setExecutionState(state: ExecutionState) {
         _executionState.value = state
     }
 
-    fun getAppChangelog() {
+    private fun getAppChangelog() {
         viewModelScope.launch(io) {
             _versionsInfo.value = Resource.Loading()
             _versionsInfo.value = getChangelogFromGitUseCase()
         }
     }
 
-    fun getCoursesVersionId() {
+    private fun getCoursesVersionId() {
         viewModelScope.launch(io) {
             _coursesVersionId.value = Resource.Loading()
             _coursesVersionId.value = getCoursesVersionIdUseCase()
         }
     }
 
-    fun getKotlinVersionId() {
+    private fun getKotlinVersionId() {
         viewModelScope.launch(io) {
             _kotlinVersionId.value = Resource.Loading()
             _kotlinVersionId.value = getKotlinVersionIdUseCase()
         }
     }
 
-    fun getCoroutineVersionId() {
+    private fun getCoroutineVersionId() {
         viewModelScope.launch(io) {
             _coroutineVersionId.value = Resource.Loading()
             _coroutineVersionId.value = getCoroutineVersionIdUseCase()
         }
     }
 
-    fun initGlobalData(context: Context) {
+    private fun initGlobalData(context: Context) {
         globalData.initGlobalData(
             versionsInfo = _versionsInfo.value.data,
             coursesVersionId = _coursesVersionId.value.data ?: DEFAULT_VERSION_ID,
@@ -101,7 +187,7 @@ class SplashViewModel @Inject constructor(
         )
     }
 
-    fun checkNewVersion() {
+    private fun checkNewVersion() {
         val appVersionCode = globalData.appVersionCode ?: DEFAULT_VERSION_CODE
         val lastVersionCode = _versionsInfo.value.data?.lastVersionCode ?: appVersionCode
         val minSupportedVersionCode =
@@ -116,12 +202,6 @@ class SplashViewModel @Inject constructor(
             else
                 NO_UPDATE_STATE
     }
-
-    fun goToUpdate(activity: MainActivity) {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(BAZAAR_URL))
-        activity.startActivity(intent)
-    }
-
 
     private fun getAppVersionCode(context: Context): Int {
         val packageInfo: PackageInfo =
